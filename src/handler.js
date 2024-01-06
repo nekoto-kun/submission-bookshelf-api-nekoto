1b0db9237b91861073c3ef1cb3af5d67e18b5fa5
Andre Kho <andreyonathan65@gmail.com>
2024-01-06 11:12:40 +0700

    Add updateBookHandler

--- src/handler.js
+++ src/handler.js
@@ -105,8 +105,66 @@ const getBookHandler = (request, h) => {
   return response
 }
 
+const updateBookHandler = (request, h) => {
+  const { bookId } = request.params
+
+  const {
+    name, year, author, summary,
+    publisher, pageCount, readPage, reading
+  } = request.payload
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku'
+    })
+
+    response.code(400)
+    return response
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount'
+    })
+
+    response.code(400)
+    return response
+  }
+
+  const updatedAt = new Date().toISOString()
+  const index = books.findIndex(book => book.id === bookId)
+
+  if (index === -1) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Id tidak ditemukan'
+    })
+
+    response.code(404)
+    return response
+  }
+
+  books[index] = {
+    ...books[index],
+    name, year, author, summary,
+    publisher, pageCount, readPage, reading,
+    updatedAt
+  }
+
+  const response = h.response({
+    status: 'success',
+    message: 'Buku berhasil diperbarui'
+  })
+
+  response.code(200)
+  return response
+}
+
 export default {
   addBookHandler,
   listBooksHandler,
-  getBookHandler
+  getBookHandler,
+  updateBookHandler
 }

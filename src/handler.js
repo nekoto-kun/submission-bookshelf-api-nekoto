6508418c4983dc7acaa222ec41f9b0a0ea9dc23c
Andre Kho <andreyonathan65@gmail.com>
2024-01-06 11:00:28 +0700

    Add first handler

---
+++ src/handler.js
@@ -0,0 +1,68 @@
+import { nanoid } from 'nanoid'
+import books from './books.js'
+
+const addBookHandler = (request, h) => {
+  const {
+    name, year, author, summary,
+    publisher, pageCount, readPage, reading
+  } = request.payload
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku'
+    })
+
+    response.code(400)
+    return response
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+    })
+
+    response.code(400)
+    return response
+  }
+
+  const id = nanoid(16)
+  const finished = pageCount === readPage
+  const insertedAt = new Date().toISOString()
+  const updatedAt = insertedAt
+
+  const newBook = {
+    id, name, year, author, summary,
+    publisher, pageCount, readPage, finished,
+    reading, insertedAt, updatedAt
+  }
+
+  books.push(newBook)
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id
+      }
+    })
+
+    response.code(201)
+    return response
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan'
+  })
+
+  response.code(500)
+}
+
+export default {
+  addBookHandler,
+}

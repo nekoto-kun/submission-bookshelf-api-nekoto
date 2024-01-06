ba1f05e72d87aae2e3de2ab8243fdef149a26fb6
Andre Kho <andreyonathan65@gmail.com>
2024-01-06 11:00:04 +0700

    Prepare routes

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const routes = [
+  {
+    method: 'POST',
+    url: '/books',
+    handler: () => { }
+  },
+  {
+    method: 'GET',
+    url: '/books',
+    handler: () => { }
+  },
+  {
+    method: 'GET',
+    url: '/books/{bookId}',
+    handler: () => { }
+  },
+  {
+    method: 'PUT',
+    url: '/books/{bookId}',
+    handler: () => { }
+  },
+  {
+    method: 'DELETE',
+    url: '/books/{bookId}',
+    handler: () => { }
+  },
+]
+
+export default routes

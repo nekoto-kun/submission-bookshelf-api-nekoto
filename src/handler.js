4d3673550b300792c4d9df4197954a79f70f8bb0
Andre Kho <andreyonathan65@gmail.com>
2024-01-06 11:04:33 +0700

    Add getBookHandler

--- src/handler.js
+++ src/handler.js
@@ -79,7 +79,34 @@ const listBooksHandler = (_, h) => {
   return response
 }
 
+const getBookHandler = (request, h) => {
+  const { bookId } = request.params
+
+  const book = books.find(book => book.id === bookId)
+
+  if (!book) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Buku tidak ditemukan'
+    })
+
+    response.code(404)
+    return response
+  }
+
+  const response = h.response({
+    status: 'success',
+    data: {
+      book
+    }
+  })
+
+  response.code(200)
+  return response
+}
+
 export default {
   addBookHandler,
-  listBooksHandler
+  listBooksHandler,
+  getBookHandler
 }

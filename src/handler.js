2eaf0638b9598efd56323febd3af02986bec16c6
Andre Kho <andreyonathan65@gmail.com>
2024-01-06 11:14:29 +0700

    Add deleteBookHandler

--- src/handler.js
+++ src/handler.js
@@ -162,9 +162,36 @@ const updateBookHandler = (request, h) => {
   return response
 }
 
+const deleteBookHandler = (request, h) => {
+  const { bookId } = request.params
+
+  const index = books.findIndex(book => book.id === bookId)
+
+  if (index === -1) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Buku gagal dihapus. Id tidak ditemukan'
+    })
+
+    response.code(404)
+    return response
+  }
+
+  books.splice(index, 1)
+
+  const response = h.response({
+    status: 'success',
+    message: 'Buku berhasil dihapus'
+  })
+
+  response.code(200)
+  return response
+}
+
 export default {
   addBookHandler,
   listBooksHandler,
   getBookHandler,
-  updateBookHandler
+  updateBookHandler,
+  deleteBookHandler
 }
